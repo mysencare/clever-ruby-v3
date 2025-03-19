7f2dae343dc93fd997d0fd4d6bf83569e664dda9
Emilio Quintana <emilioquintana90@gmail.com>
2025-03-19 16:00:51 -0500

    Add missing build_from_hash function to models

--- lib/clever-ruby-v3/models/schools_updated.rb
+++ lib/clever-ruby-v3/models/schools_updated.rb
@@ -106,5 +106,35 @@ module CleverV3
     def hash
       [data, previous_attributes].hash
     end
+
+    # Builds the object from hash
+    # @param [Hash] attributes Model attributes in the form of hash
+    # @return [Object] Returns the model itself
+    def self.build_from_hash(attributes)
+      new.build_from_hash(attributes)
+    end
+
+    # Builds the object from hash
+    # @param [Hash] attributes Model attributes in the form of hash
+    # @return [Object] Returns the model itself
+    def build_from_hash(attributes)
+      return nil unless attributes.is_a?(Hash)
+      super(attributes)
+      self.class.openapi_types.each_pair do |key, type|
+        if type =~ /\AArray<(.*)>/i
+          # check to ensure the input is an array given that the attribute
+          # is documented as an array but the input is not
+          if attributes[self.class.attribute_map[key]].is_a?(Array)
+            self.send("#{key}=", attributes[self.class.attribute_map[key]].map { |v| _deserialize($1, v) })
+          end
+        elsif !attributes[self.class.attribute_map[key]].nil?
+          self.send("#{key}=", _deserialize(type, attributes[self.class.attribute_map[key]]))
+        elsif attributes[self.class.attribute_map[key]].nil? && self.class.openapi_nullable.include?(key)
+          self.send("#{key}=", nil)
+        end
+      end
+
+      self
+    end
   end
 end
